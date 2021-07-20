5adafe1797df8475e904444a524932345430fe21
= <=>
2021-07-20 23:58:02 +0800

    add favicon

--- src/api/main.go
+++ src/api/main.go
@@ -5,21 +5,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/thinkerou/favicon"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 
+	router.Use(favicon.New("./favicon.ico"))
+	//router.StaticFile("/favicon.ico", "assets/favicon.ico")
 	router.LoadHTMLGlob("templates/*")
 
 	router.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main WebSite",
+		})
+	})
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
 	})
 
