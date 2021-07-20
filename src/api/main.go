f8b40732ca11e72876e14b039141bdab73b8ff3f
kkou <a@b.c>
2021-07-20 22:29:09 +0800

    initial commit

---
+++ src/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main WebSite",
+		})
+	})
+
+	log.Print("web is running...")
+	router.Run(":8000")
+}
