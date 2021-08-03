845c8c340d61e911093b5120159ad7b7810c7e7d
kkou <a@b.c>
2021-08-03 11:10:24 +0800

    extract layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,70 +1,14 @@
 import './App.css';
+import Container from './layout/container';
+import Footer from './layout/footer';
+import Header from './layout/header';
 
 function App() {
   return (
     <div>
-      <header>
-      <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
-          <div className="container">
-              <a className="navbar-brand" asp-area="" asp-controller="Home" asp-action="Index">signalr</a>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
-                      aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
-                  <ul className="navbar-nav flex-grow-1">
-                      <li className="nav-item">
-                          <a className="nav-link text-dark" asp-area="" asp-controller="Home" asp-action="Index">Home</a>
-                      </li>
-                      <li className="nav-item">
-                          <a className="nav-link text-dark" asp-area="" asp-controller="Home" asp-action="Privacy">Privacy</a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-        </nav>
-      </header>
-      <div className="container">
-          <main role="main" className="pb-3">
-              <div className="text-center">
-                  <h1 className="display-4">Welcome</h1>
-              </div>
-              <div className="container">
-                  <div className="row">&nbsp;</div>
-                  <div className="row">
-                      <div className="col-2">User</div>
-                      <div className="col-4"><input type="text" id="userInput" /></div>
-                  </div>
-                  <br/>
-                  <div className="row">
-                      <div className="col-2">Message</div>
-                      <div className="col-4"><input type="text" id="messageInput" /></div>
-                  </div>
-                  <div className="row">&nbsp;</div>
-                  <div className="row">
-                      <div className="col-6">
-                          <input type="button" id="sendButton" value="Send Message" />
-                      </div>
-                  </div>
-              </div>
-              <div className="row">
-                  <div className="col-12">
-                      <hr />
-                  </div>
-              </div>
-              <div className="row">
-                  <div className="col-6">
-                      <ul id="messagesList"></ul>
-                  </div>
-              </div>
-      </main>
-      </div>
-
-      <footer className="border-top footer text-muted">
-          <div className="container">
-              &copy; 2021 HomeSite - <a>Privacy</a>
-          </div>
-      </footer>
+        <Header/>
+        <Container/>
+        <Footer/>
     </div>
   );
 }
---
+++ web/src/layout/footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+	return (
+		<footer className="border-top footer text-muted">
+		<div className="container">
+			&copy; 2021 HomeSite - <a>Privacy</a>
+		</div>
+	</footer>
+	);
+}
+
+export default Footer;
